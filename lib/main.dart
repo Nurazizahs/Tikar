692ac1f017bd0b2b8db85938a8c16b1fe37ba4cf
4naufalulum <201240001047@unisnu.ac.id>
2023-08-23 12:22:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TIKAR',
+      theme: AppTheme.theme,
+      home: const MyHomePage(title: 'TIKAR'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Pallete.barColor,
+        title: Text(widget.title),
+      ),
+      body: const Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(
+          Icons.add,
+          color: Pallete.whiteColor,
+          ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
