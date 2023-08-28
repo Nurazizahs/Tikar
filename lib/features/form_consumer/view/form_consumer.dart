9bbd6ee989dc90f18ad19fbae63b6a6dacb1e120
4naufalulum <201240001047@unisnu.ac.id>
2023-08-28 12:15:41 +0700

    membuat list view di dashboard dan form penitipan

--- lib/features/form_consumer/view/form_consumer.dart
+++ lib/features/form_consumer/view/form_consumer.dart
@@ -10,39 +10,206 @@ class FormView extends StatefulWidget {
 }
 
 class _FormViewState extends State<FormView> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Pallete.barColor,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end, 
           children: [
             Text(
               'Form',
             )
-          ]
-        ),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.inbox,
-              color: Pallete.barColor,
-              size: 28,
-            ),
-            Text(
-              'Formulir Penitipan Barang',
-              style: TextStyle(
-                color: Pallete.barColor,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
           ],
         ),
-        
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.inbox,
+                    color: Pallete.barColor, // Ganti dengan warna yang sesuai
+                    size: 28,
+                  ),
+                  Text(
+                    'Formulir Penitipan Barang',
+                    style: TextStyle(
+                      color: Pallete.barColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Jenis Barang',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Model Barang',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Ukuran Barang',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Berat Barang',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Tempat Penjemputan',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Tujuan Pengantaran',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Kode Promo',
+                  labelStyle: TextStyle(color: Pallete.barColor),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.barColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Pallete.blackColor),
+                  ),
+                ),
+                style: TextStyle(color: Pallete.blackColor),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                    fillColor: MaterialStateColor.resolveWith((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return Pallete.barColor;
+                      }
+                      return Pallete.barColor;
+                    }),
+                  ),
+                  Flexible(
+                    child: Text(
+                      'Apakah infromasi yang anda masukkan sudah benar',
+                      style: TextStyle(
+                        color: Pallete.barColor,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi yang akan dijalankan saat tombol ditekan
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Pallete.barColor,
+                  onPrimary: Pallete.searchBarColor,
+                ),
+                child: Text(
+                  'Lanjut',
+                  style: TextStyle(
+                    color: Pallete.backgroundColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/features/home_consumer/view/home_view.dart
+++ lib/features/home_consumer/view/home_view.dart
@@ -12,6 +12,21 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  List<Map<String, String>> items = [
+  {
+    'name': 'Maulana Hadi',
+    'object': 'Kursi',
+    'from': 'Dari Jepara',
+    'to': 'Ke Jakarta',
+  },
+  {
+    'name': 'Bagas Tri',
+    'object': 'Meja',
+    'from': 'Dari Surabaya',
+    'to': 'Ke Bandung',
+  },
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,19 +57,86 @@ class _HomeViewState extends State<HomeView> {
           ),
         ),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Selamat Datang di Aplikasi TIKAR',
-              style: TextStyle(
-                color: Pallete.barColor,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Selamat Datang di Aplikasi TIKAR',
+                style: TextStyle(
+                  color: Pallete.barColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Pallete.barColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.person),
+                                  SizedBox(width: 8),
+                                  Text(items[index]['name'] ?? ''),
+                                ],
+                              ),
+                              Text(items[index]['object'] ?? ''),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          color: Pallete.backgroundColor,
+                          thickness: 1,
+                        ),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(items[index]['from'] ?? ''),
+                              Text(items[index]['to'] ?? ''),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                              
+                              },
+                              child: Text(
+                                'Lihat Selengkapnya',
+                                style: TextStyle(
+                                  color: Pallete.whiteColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
